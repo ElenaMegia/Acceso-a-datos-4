fe64297048c75e44ea5651f39b83019906ee15ec
ElenaMegia <emegican@myuax.com>
2022-11-24 19:24:14 +0100

    Initial commit

---
+++ src/main/java/com/example/accesoadatos4/AccesoADatos4Application.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoADatos4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoADatos4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accesoadatos4/AccesoADatos4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoADatos4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
